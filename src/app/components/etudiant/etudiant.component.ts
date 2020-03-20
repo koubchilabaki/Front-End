a8252685cc0cb364df08b3eb0071f53c513b9906
Adil Oulghard <adil.oulghard@gmail.com>
2020-03-20 13:46:37 +0100

    Pagination etudiant

--- src/app/components/etudiant/etudiant.component.ts
+++ src/app/components/etudiant/etudiant.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {SectionComponent} from '../section/section.component';
 import {BsModalService} from 'ngx-bootstrap';
 import {EtudiantService} from '../../services/etudiant.service';
 import {Etudiant} from '../../models/etudiant';
+import {MatTableDataSource} from '@angular/material/table';
+import {Qualificatif} from '../../models/qualificatif';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-etudiant',
@@ -12,6 +16,12 @@ import {Etudiant} from '../../models/etudiant';
 })
 export class EtudiantComponent implements OnInit {
     etudiants: Etudiant[];
+
+  displayedColumns: string[] = ['sexe', 'nom', 'prenom','email','nationalite','universiteOrigine'];
+  dataSource: MatTableDataSource<Etudiant>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
   constructor(private router: Router, private route: ActivatedRoute
     ,private etudiantService: EtudiantService
     ,private secComp: SectionComponent
@@ -20,11 +30,30 @@ export class EtudiantComponent implements OnInit {
   ngOnInit(): void {
     let codeFormation = this.route.snapshot.paramMap.get('codeFormation');
     let anneeUniversitaire = this.route.snapshot.paramMap.get('anneeUniversitaire');
-    this.etudiantService.getEtudiantsByACodeFormation(anneeUniversitaire,codeFormation).subscribe((etudiants)=>{
+    this.paginator._intl.itemsPerPageLabel = 'Enregistrements par page:';
+    this.getEtudiantsByACodeFormation(anneeUniversitaire, codeFormation);
+  }
+
+  getEtudiantsByACodeFormation(annee:string, code:string){
+    this.etudiantService.getEtudiantsByACodeFormation(annee , code).subscribe((etudiants)=>{
       this.etudiants = etudiants;
+
+      this.dataSource = new MatTableDataSource(this.etudiants);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     }, (error) => {
       console.log(error);
     });
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 
 }
